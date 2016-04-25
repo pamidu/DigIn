3aa3e2a78aec194f491677f17ac5d0bf67f693ce
senal kumarage <senalkumarage@outlook.com>
2016-04-25 14:49:49 +0530

    Fixed social media fb issues
    Signed-off-by: senal kumarage <senalkumarage@outlook.com>

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $fullHost = strtolower($_SERVER['HTTP_HOST']);
 switch ($fullHost) {
 
     case $mainDomain:
-        if(!isset($_COOKIE["securityToken"])){
+        if(isset($_COOKIE["securityToken"])){
 
               include ("index1.php");
         }else{

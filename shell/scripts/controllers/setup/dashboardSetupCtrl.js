6e74f2d0a0b9a2e55fa1c01f1b393dd3ff3a07ba
gevindu1990 <gevindu@duosoftware.com>
2016-08-26 18:08:11 +0530

    widget validations account settings image

--- shell/scripts/controllers/setup/dashboardSetupCtrl.js
+++ shell/scripts/controllers/setup/dashboardSetupCtrl.js
@@ -44,6 +44,12 @@ routerApp.controller('dashboardSetupCtrl', function ($scope, $mdDialog, $locatio
 		  $state.go('home.welcomeSearch');
     };
 
+    $scope.userSetingsUrl =$rootScope.image ? $rootScope.image : "styles/css/images/setting/account_settings.png";
+    $rootScope.$watch('image', function(newValue, oldValue) {
+        $scope.userSetingsUrl =$rootScope.image;
+    });
+
+
     //theme colors array
     $scope.colorArr = [{value: '#F44336'}, {value: '#E91E63'}, {value: '#9C27B0'}, {value: '#673AB7'}, {value: '#3F51B5'}, {value: '#2196F3'}, {value: '#03A9F4'}, {value: '#00BCD4'}, {value: '#009688'}, {value: '#4CAF50'}, {value: '#8BC34A'}, {value: '#CDDC39'}, {value: '#FFEB3B'}, {value: '#FFC107'}, {value: '#FF9800'}, {value: '#FF5722'}, {value: '#795548'}, {value: '#9E9E9E'}, {value: '#607D8B'}];
     

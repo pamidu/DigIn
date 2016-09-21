36ed450b4f78040533b3a0cccb74635c0ebe50f2
Dilani <dilani@duosoftware.com>
2016-09-21 16:55:02 +0530

    removed notifications, moved directives in querybuilder.js to a separate directives file.

--- shell/scripts/controllers/query-builder/queryBuilderCtrl.js
+++ shell/scripts/controllers/query-builder/queryBuilderCtrl.js
@@ -3492,36 +3492,4 @@ routerApp.directive('ngColorPicker', ['ngColorPickerConfig',function(ngColorPick
         };
         //#damith
         //create custom query design catch syntax error
-    }).directive("markable", function() {
-        return {
-            link: function(scope, elem, attrs) {
-                elem.on("click", function() {
-                    elem.addClass("active-condition");
-                });
-            }
-        };
-    }).directive("removeMarkable", function() {
-        return {
-            link: function(scope, elem, attrs) {
-                elem.on("click", function() {
-                    elem.addClass("de-active-condition");
-                });
-            }
-        };
-    }).directive("selectedSettingIcon", function() {
-        return {
-            link: function(scope, elem, attrs) {
-                elem.on("click", function() {
-                    elem.addClass("icon-select-ri1");
-                });
-            }
-        };
-    }).directive("findSelectTab", function() {
-        return {
-            link: function(scope, elem, attrs) {
-                elem.on("click", function() {
-                    elem.addClass("icon-select-ri1");
-                });
-            }
-        };
-    });
+    })

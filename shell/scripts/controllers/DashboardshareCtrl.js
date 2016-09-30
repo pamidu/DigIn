cad2c5b5c061f60e9afad6d0359b862e03c72d13
gevindu1990 <gevindu@gmail.com>
2016-09-30 17:25:11 +0530

    DUODIGIN-1160, DUODIGIN-1154, DUODIGIN-1150, DUODIGIN-1048

--- shell/scripts/controllers/DashboardshareCtrl.js
+++ shell/scripts/controllers/DashboardshareCtrl.js
@@ -49,17 +49,26 @@ routerApp.controller('shareCtrl', ['Socialshare', '$scope','$mdDialog','widget',
 
 
     $scope.getreturnSocial = function(url,provider){
-        Socialshare.share({
-                      'provider': provider,
-                      'attrs': {
+        //var url = 'http://prod.digin.io/digin_data/digin_user_data/1fe2dfa9c6c56c8492b9c78107eb5ae3/nordirisrhytacom.prod.digin.io/DPs/4.jpg';
+        if(provider == "pinterest"){
+            window.open('https://pinterest.com/pin/create/button/?url='+url+'','_blank');
+        }else if(provider == "tumblr"){
+             window.open('http://www.tumblr.com/share/link?url='+url+'','_blank');
+        }
+        else{
+            Socialshare.share({
+              'provider': provider,
+              'attrs': {
 
-                        'socialshareUrl': url,
-                        'socialsharePopupHeight':'400',
-                        'socialsharePopupWidth':'400'
-                        
+                'socialshareUrl': url,
+                'socialsharePopupHeight':'400',
+                'socialsharePopupWidth':'400'
 
-                      }
-                });
+
+            }
+        });
+        } 
+        
     }
   
 }]);

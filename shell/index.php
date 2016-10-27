a9feb9d3b6fe0ceb91c051ca8a2077ddc00e34a1
sajeetharan <sajeetharan@duosoftware.com>
2016-10-27 21:22:40 +0530

    Changes

--- shell/index.php
+++ shell/index.php
@@ -9,8 +9,8 @@
         require_once($_SERVER['DOCUMENT_ROOT'] . "/2016-10-27/include/session.php");
      }
      else{
-        require_once($_SERVER['DOCUMENT_ROOT'] . "/2016-10-27/Digin/include/config.php");
-        require_once($_SERVER['DOCUMENT_ROOT'] . "/2016-10-27/Digin/include/session.php");
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/Digin/include/config.php");
+        require_once($_SERVER['DOCUMENT_ROOT'] . "/Digin/include/session.php");
      }
 
     INTS();

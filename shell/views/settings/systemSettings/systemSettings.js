05cd966e17c601295b964e428c9c9ea2e3b4c299
Chamila <chamila@duosoftware.com>
2016-11-03 10:23:09 +0530

    Validation fixes

--- shell/views/settings/systemSettings/systemSettings.js
+++ shell/views/settings/systemSettings/systemSettings.js
@@ -1,4 +1,6 @@
-routerApp.controller('systemSettingsCtrl',[ '$scope','$rootScope','$mdDialog', 'notifications','$http','Digin_Engine_API','Digin_Domain','$state','ProfileService', function ($scope,$rootScope,$mdDialog,notifications,$http,Digin_Engine_API,Digin_Domain,$state,ProfileService){
+routerApp.controller('systemSettingsCtrl',[ '$scope','$rootScope','$mdDialog', 'notifications','$http','Digin_Engine_API','Digin_Domain','$state','ProfileService','userAdminFactory', function ($scope,$rootScope,$mdDialog,notifications,$http,Digin_Engine_API,Digin_Domain,$state,ProfileService,userAdminFactory){
+
+    userAdminFactory.getUserLevel();
 
     $scope.sizes = [
         "5",
@@ -50,89 +52,99 @@ routerApp.controller('systemSettingsCtrl',[ '$scope','$rootScope','$mdDialog', '
     }
     
     
-	
-	
+    
+    
 //#update refresh widget settings
     $scope.updateSettings = function () {
-
-        if ($scope.cacheLifetime == undefined) {
-            notifications.toast('0', 'Invalid cache lifetime settings.');
-            return;
-        }
-        else if ($scope.noOfWidget == undefined) {
-            notifications.toast('0', 'Invalid widgets settings!');
-            return;
-        }
-        else if ($scope.reqLimit == undefined) {
-            notifications.toast('0', 'Invalid request limit settings.');
-            return;
-        }
-        else if ($scope.cacheLifetime <= 0) {
-            notifications.toast('0', 'Invalid cache lifetime settings.');
-            return;
-        }
-        else {
-        }
-
-        var userInfo = JSON.parse(decodeURIComponent(getCookie('authData')));
-        
-        //#chk undefined values
-        var dp_name="";
-        var logo_name="";
-        var components; var userRole; var cacheLifetime; var widgetLimit; var themeConfig; var queryLimit;
-        components = JSON.parse($rootScope.userSettings.components);
-        components.saveExplicit = $scope.components;
-        components = JSON.stringify(components);
-        if($rootScope.userSettings.user_role==undefined) {userRole="";} else {userRole=$rootScope.userSettings.user_role}
-        if($scope.cacheLifetime==undefined) {cacheLifetime=0;} else {cacheLifetime=parseInt($scope.cacheLifetime)}
-        if($scope.noOfWidget==undefined) {widgetLimit=6;} else {widgetLimit=parseInt($scope.noOfWidget)}
-        if($scope.reqLimit==undefined) {queryLimit=0;} else {queryLimit=parseInt($scope.reqLimit)}
-        if($rootScope.userSettings.dp_path==undefined) {dp_name="";} else {dp_name=$rootScope.userSettings.dp_path.split("/").pop();}
-        if($rootScope.userSettings.logo_path==undefined) {logo_name="";} else {logo_name=$rootScope.userSettings.logo_path.split("/").pop();}
-        if($rootScope.userSettings.theme_config==undefined) {themeConfig="";} else {themeConfig=$rootScope.userSettings.theme_config}     
-        
-        //#store to user settings---------------------
-        $scope.settings = {
-            "email": userInfo.Email,
-            "components": components,
-            "user_role":userRole,
-            "cache_lifetime":cacheLifetime,
-            "widget_limit": widgetLimit,
-            "query_limit": queryLimit,
-            "logo_name": logo_name,
-            "dp_name" : dp_name,
-            "theme_config": themeConfig
-        }
-    
-        $http({
-            method: 'POST',
-            url: Digin_Engine_API + 'store_user_settings/',
-            data: angular.toJson($scope.settings),
-            headers: {
-                'Content-Type': 'application/json',
-                'SecurityToken': userInfo.SecurityToken
+            if($rootScope.userLevel=='user'){
+                   notifications.toast('0','You are not permitted to do this operation, allowed only for administrator'); 
+                   return;
             }
-        })
-            .success(function (response) {
-                //alert("Success...!");
-                notifications.toast('1', 'User settings saved successfully.');
-                var userInfo = JSON.parse(decodeURIComponent(getCookie('authData')));
-                $http.get(Digin_Engine_API + 'get_user_settings?SecurityToken=' + userInfo.SecurityToken + '&Domain=' + Digin_Domain)
-                .success(function (data) {
-                $rootScope.userSettings=data.Result;  
-                ProfileService.widget_limit = data.Result.widget_limit;       
-                })
-                .error(function (data) {        
-                });
+            else if ($scope.cacheLifetime == undefined) {
+                notifications.toast('0', 'Invalid cache lifetime settings.');
+                return;
+            }
+            else if ($scope.noOfWidget == undefined) {
+                notifications.toast('0', 'Invalid widgets settings!');
+                return;
+            }
+            else if ($scope.reqLimit == undefined) {
+                notifications.toast('0', 'Invalid request limit settings.');
+                return;
+            }
+            else if ($scope.cacheLifetime <= 0) {
+                notifications.toast('0', 'Invalid cache lifetime settings.');
+                return;
+            }
+            else {
+            }
+
+            var userInfo = JSON.parse(decodeURIComponent(getCookie('authData')));
+            
+            //#chk undefined values
+            var dp_name="";
+            var logo_name="";
+            var components; var userRole; var cacheLifetime; var widgetLimit; var themeConfig; var queryLimit;
+            components = JSON.parse($rootScope.userSettings.components);
+            components.saveExplicit = $scope.components;
+            components = JSON.stringify(components);
+            if($rootScope.userSettings.user_role==undefined) {userRole="";} else {userRole=$rootScope.userSettings.user_role}
+            if($scope.cacheLifetime==undefined) {cacheLifetime=0;} else {cacheLifetime=parseInt($scope.cacheLifetime)}
+            if($scope.noOfWidget==undefined) {widgetLimit=6;} else {widgetLimit=parseInt($scope.noOfWidget)}
+            if($scope.reqLimit==undefined) {queryLimit=0;} else {queryLimit=parseInt($scope.reqLimit)}
+            if($rootScope.userSettings.dp_path==undefined) {dp_name="";} else {dp_name=$rootScope.userSettings.dp_path.split("/").pop();}
+            if($rootScope.userSettings.logo_path==undefined) {logo_name="";} else {logo_name=$rootScope.userSettings.logo_path.split("/").pop();}
+            if($rootScope.userSettings.theme_config==undefined) {themeConfig="";} else {themeConfig=$rootScope.userSettings.theme_config}     
+            
+            //#store to user settings---------------------
+            $scope.settings = {
+                "email": userInfo.Email,
+                "components": components,
+                "user_role":userRole,
+                "cache_lifetime":cacheLifetime,
+                "widget_limit": widgetLimit,
+                "query_limit": queryLimit,
+                "logo_name": logo_name,
+                "dp_name" : dp_name,
+                "theme_config": themeConfig
+            }
+        
+            $http({
+                method: 'POST',
+                url: Digin_Engine_API + 'store_user_settings/',
+                data: angular.toJson($scope.settings),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'SecurityToken': userInfo.SecurityToken
+                }
             })
-            .error(function (error) {
-                notifications.toast('0', 'Please re-check the settings.');
-            });
+                .success(function (response) {
+                    //alert("Success...!");
+                    notifications.toast('1', 'User settings saved successfully.');
+                    var userInfo = JSON.parse(decodeURIComponent(getCookie('authData')));
+                    $http.get(Digin_Engine_API + 'get_user_settings?SecurityToken=' + userInfo.SecurityToken + '&Domain=' + Digin_Domain)
+                    .success(function (data) {
+                    $rootScope.userSettings=data.Result;  
+                    ProfileService.widget_limit = data.Result.widget_limit;       
+                    })
+                    .error(function (data) {        
+                    });
+                })
+                .error(function (error) {
+                    notifications.toast('0', 'Please re-check the settings.');
+                });
+
     };
 
 
     //#Clear cache
     $scope.clearCache = function () {
+        
+        if($rootScope.userLevel=='user'){
+                   notifications.toast('0','You are not permitted to do this operation, allowed only for administrator'); 
+                   return;
+            }
+            
         var confirm = $mdDialog.confirm()
             .title('Do you want to clear cache ?')
             .targetEvent(event)
@@ -170,7 +182,7 @@ routerApp.controller('systemSettingsCtrl',[ '$scope','$rootScope','$mdDialog', '
     //*Settings routing ---------------- 
     var slide = false;
     $scope.route = function (state) {
-		  $state.go('home.welcomeSearch');
+          $state.go('home.welcomeSearch');
     };
 
 

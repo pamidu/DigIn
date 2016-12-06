fe87703d09305aa3b2287dc2e2651166b4b37a54
Dilani <dilani@duosoftware.com>
2016-12-06 16:29:03 +0530

    DUODIGIN-1298

--- shell/views/settings/datasourceSettings/datasourceSettingsCtrl.js
+++ shell/views/settings/datasourceSettings/datasourceSettingsCtrl.js
@@ -17,8 +17,15 @@ routerApp.controller('DatasourceSettingsCtrl',[ '$scope','$state','$rootScope','
 	$scope.databaseType = "";
 	$scope.databaseName = "";
 	$scope.authType = "";
+	$scope.connName = "";
 	$scope.loginStatus = false;
+	$scope.submitLogin = false;
 	$scope.testStatus = false;
+	$scope.testRequest = false;
+	$scope.connectionStatus = false;
+	$scope.connectionNotAvailable = false;
+	$scope.saveRequest = false;
+	$scope.existingConnections = [];
 	$scope.stepData = [{
 	    step: 1,
 	    completed: false,
@@ -50,9 +57,16 @@ routerApp.controller('DatasourceSettingsCtrl',[ '$scope','$state','$rootScope','
 	{
 	    $scope.selectedStep++;
 	}
+	$scope.decrementStep = function()
+	{
+		$scope.selectedStep--;
+	}
 	$scope.moveToPreviousStep = function()
 	{
-	    $scope.selectedStep--;
+	    //call getAllConnections
+	    $scope.existingConnections = [];
+	    $scope.decrementStep();
+	    $scope.getAllconnections();
 	}
 	// route to home
 	$scope.goHome = function()
@@ -63,24 +77,28 @@ routerApp.controller('DatasourceSettingsCtrl',[ '$scope','$state','$rootScope','
 	//get all connections
 	$scope.getAllconnections = function()
 	{
+		$scope.connectionStatus = false;
 		datasourceFactory.getAllConnections(securityToken).success(function(data){
-			console.log(data);
-			data.Is_Success = false;
 			if(data.Is_Success)
-			{
+			{	
+				$scope.connectionStatus = true;
+				$scope.existingConnections = data.Result;
+				if($scope.existingConnections.length == 0)
+				{
+					$scope.connectionNotAvailable = true;
+				}
 				notifications.toast('1',data.Custom_Message);
 			} else 
 			{
+				$scope.connectionStatus = true;
 				notifications.toast('0',data.Custom_Message);
 			}
 		}).error(function(data){
 			console.log(data);
+			$scope.connectionStatus = true;
 			notifications.toast('0','Request failed.Please try again.');
 		});
 	}
-
-	
-
 	// Create new connection
 	$scope.createConnection = function()
 	{
@@ -89,7 +107,7 @@ routerApp.controller('DatasourceSettingsCtrl',[ '$scope','$state','$rootScope','
 			$scope.incrementStep();
 		} else if ($scope.selectedStep == 2)
 		{
-			$scope.moveToPreviousStep();
+			$scope.decrementStep();
 		}
 		$scope.clearDetails();
 	}
@@ -103,35 +121,149 @@ routerApp.controller('DatasourceSettingsCtrl',[ '$scope','$state','$rootScope','
 		$scope.databaseType = "";
 		$scope.databaseName = "";
 		$scope.authType = "";
+		$scope.connName = "";
 		$scope.loginStatus = false;
-		$scope.testStatus = false;		
+		$scope.testStatus = false;
+		$scope.testRequest = false;
+		$scope.submitLogin = false;
+		$scope.connectionStatus = true;
+		$scope.saveRequest = false;
+		$scope.connectionNotAvailable = false;
 	}
 
+	// Submit user details to get the database names
 	$scope.submitUserDetails = function()
 	{
+		// validation
+		if ($scope.host == '' || $scope.username == '' || $scope.password == '' || $scope.port == '')
+		{
+			notifications.toast('0', 'Please fill in all required fields.');
+			return;
+		}
 		var reqParam = {
 			host: $scope.host, 
 			username: $scope.userName,
 			password: $scope.password,
 			port: $scope.port
 		}
+		$scope.submitLogin = true;
+		$scope.loginStatus = false;
+		$scope.testStatus = false;
+		$scope.testRequest = false;
 		datasourceFactory.getAllDatabases(securityToken,reqParam).success(function(data){
 			if(data.Is_Success)
 			{
-				loginStatus = true;
+				$scope.databaseType = "";
+				$scope.databaseName = "";
+				$scope.authType = "";
+				$scope.connName = "";
+				$scope.loginStatus = true;
+				$scope.submitLogin = false;
+				$scope.dbNames = data.Result.sort();
 				notifications.toast('1',data.Custom_Message);
-			} else 
+			} else
 			{
+				$scope.submitLogin = false;
 				notifications.toast('0',data.Custom_Message);
 			}
 		}).error(function(data){
-			console.log(data);
+			$scope.submitLogin = false;
 			notifications.toast('0','Request failed.Please try again.');
 		});
 	}
-	// Get user details
-	// $scope.submitUserDetails = function()
-	// {
-		
-	// }
+
+	//Test the connection with the selected database name
+	$scope.testConnection = function()
+	{
+		var isExist = false;
+		// Add validation
+		if ($scope.databaseName == '' || $scope.connName == '' || $scope.databaseType == '' || $scope.host == '' || $scope.username == '' || $scope.password == '' || $scope.port == '')
+		{
+			notifications.toast('0', 'Please fill in all required fields.');
+			return;
+		}
+		angular.forEach($scope.existingConnections,function(key){
+			console.log(key);
+			if (key.connection_name == $scope.connName && key.database_name == $scope.databaseName && key.host_name == $scope.host)
+			{
+				notifications.toast('1','connection already exist');
+				$scope.port = key.port;
+				isExist = true;
+			}
+		})
+		// return if the conenction already exists
+		if (isExist) return;
+		var reqParam = {
+			host: $scope.host, 
+			username: $scope.userName,
+			password: $scope.password,
+			port: $scope.port,
+			databaseName: $scope.databaseName
+		}
+		$scope.testRequest = true;
+		$scope.testStatus = false;
+		datasourceFactory.testConnection(securityToken,reqParam).success(function(data){
+			if(data.Is_Success)
+			{
+				$scope.testRequest = false;
+				$scope.testStatus = true;
+				notifications.toast('1',data.Custom_Message);
+			} else 
+			{
+				$scope.testRequest = false;
+				notifications.toast('0',data.Custom_Message);
+			}
+		}).error(function(data){
+			$scope.testRequest = false;
+			notifications.toast('0','Request failed.Please try again.');
+		});
+	}
+
+	//Save the created connection
+	$scope.saveConnection = function()
+	{
+		var reqParam = {
+			host_name: $scope.host,
+			user_name: $scope.userName,
+			password: $scope.password,
+			port: $scope.port,
+			database_name: $scope.databaseName,
+			connection_type: "MSSQL",
+			connection_name: $scope.connName,
+			ds_config_id: null,
+			others: {}
+		}
+		$scope.saveRequest = true;
+		datasourceFactory.saveConnection(securityToken,reqParam).success(function(res){
+			if(res.Is_Success)
+			{
+				// Call getAllConnections to get the connections after saving 
+				datasourceFactory.getAllConnections(securityToken).success(function(data){
+					if(data.Is_Success)
+					{	
+						$scope.saveRequest = false;
+						$scope.incrementStep();
+						notifications.toast('1',res.Custom_Message);
+						$scope.existingConnections = data.Result;
+					} else 
+					{
+						$scope.saveRequest = false;
+						$scope.incrementStep();
+						notifications.toast('1',res.Custom_Message);
+					}
+				}).error(function(data){
+					$scope.saveRequest = false;
+					$scope.incrementStep();
+					notifications.toast('1',res.Custom_Message);
+				});
+			} else 
+			{
+				$scope.saveRequest = false;
+				notifications.toast('0',res.Custom_Message);
+			}
+		}).error(function(data){
+			notifications.toast('0','Request failed.Please try again.');
+		});
+	}
+
 }]);

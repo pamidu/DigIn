4b25749cb8c1de65b017bfad88adeee592443345
sajeetharan <sajeetharan@duosoftware.com>
2017-01-04 16:55:05 +0530

    Removed and updates

--- shell/index.php
+++ shell/index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-
 <?php 
-
     //var_dump($_SERVER['DOCUMENT_ROOT']) ; exit();
-
      if ($_SERVER['DOCUMENT_ROOT']=="/var/www/html"){
         require_once($_SERVER['DOCUMENT_ROOT'] . "/include/config.php");
         require_once($_SERVER['DOCUMENT_ROOT'] . "/include/session.php");
@@ -12,24 +9,19 @@
         require_once($_SERVER['DOCUMENT_ROOT'] . "/Digin/include/config.php");
         require_once($_SERVER['DOCUMENT_ROOT'] . "/Digin/include/session.php");
      }
-
     INTS();
-
     if(!isset($_COOKIE["securityToken"])){
         //var_dump($mainDomain);
         header("Location: http://".$mainDomain."/entry");
         exit(); 
     }
-
 ?>
-
 <html ng-app="DuoDiginRt" ng-controller="NavCtrl">
 <head>
     <title>DigIn - Beyond BI</title>
     <link rel="shortcut icon" href="./styles/css/images/innerlogo.ico" type="image/png">
     <link rel="stylesheet" href="styles/css/commonstyle.css">
     <link rel="stylesheet" href="styles/css/login.css">    <!--  new changes  -->
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="">
@@ -41,15 +33,8 @@
           content="259839742765-nq163rith421537lnfjt5dor1gr8c3jn.apps.googleusercontent.com">
     <link rel="shortcut icon" href="styles/css/images/innerlogo.ico">
     <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css"/> 
-    <!--link rel="stylesheet" href="bower_components/bootstrap/less/glyphicons.less"/>
-    <link rel="stylesheet" type="text/css" href="bower_components/angular-bootstrap-colorpicker/css/colorpicker.css"-->
     <link rel="stylesheet" href="bower_components/angular-material/angular-material.css"> 
-
-    <!-- <link rel="stylesheet" href="http://ajax.googleapis.com/ajax/libs/angular_material/0.9.4/angular-material.min.css"> -->
     <link rel="stylesheet" type="text/css" href="bower_components/codemirror/lib/codemirror.css">
-    <!-- <link href="styles/css/material-wfont.css" rel="stylesheet">-->
-    <!--link rel="stylesheet" href="http://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"-->
-    <!--link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/css/bootstrap.min.css"-->
     <link rel="stylesheet" type="text/css" href="styles/css/directive_library.css">
     <link rel="stylesheet" type="text/css" href="styles/css/pivot.css"/>
     <!-- angular gridster -->
@@ -91,24 +76,18 @@
     <link rel="stylesheet" href="bower_components/notification/ng-Toast/ngToast.min.css" type="text/css"/>
     <link rel="stylesheet" href="styles/css/hover.css" type='text/css'/>
     <link rel="stylesheet" href="bower_components/angular-ui/angular-ui-select2/select2-bootstrap.min.css" type='text/css'/>
- 
     <link rel="stylesheet" href="bower_components/tag-input/ng-tags-input.min.css"/>
     <link rel="stylesheet" href="styles/css/customer.css" type='text/css'/>
     <link rel="stylesheet" href="bower_components/v-accordion/v-accordion.min.css" type='text/css'/>
-    <link rel="stylesheet" href="styles/css/customer-1.0.css" type='text/css'/>
-    <link rel="stylesheet" href="styles/css/socialMedFb.css" type="text/css"/>
+    <link rel="stylesheet" href="styles/css/customer-1.0.css" type='text/css'/>  
     <link rel="stylesheet" href="styles/css/md-steppers.css" type="text/css"/>
     <link href='https://fonts.googleapis.com/css?family=Ek+Mukta:200,300' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700,300,100' rel='stylesheet' type='text/css'>
-    <link href='http://fonts.googleapis.com/css?family=Dosis:700' rel='stylesheet' type='text/css'>
-   
+    <link href='http://fonts.googleapis.com/css?family=Dosis:700' rel='stylesheet' type='text/css'>   
     <!-- image crop -->
     <link rel="stylesheet" href="bower_components/imgcrop/ng-img-crop.css"/> 
-
-
     <!-- <link href="http://vjs.zencdn.net/5.8.0/video-js.css" rel="stylesheet"> -->
-    <script type="text/javascript" src="//platform.linkedin.com/in.js">
-        
+    <script type="text/javascript" src="//platform.linkedin.com/in.js">        
         //api_key: 78itfewa96mm93   //2.33
         api_key: 816rs443i7o0rf
         authorize: true
@@ -121,10 +100,7 @@
             position: absolute !important;
         }
     </style>
-    <!--<style rel="stylesheet" href="styles/css/init-dash-setup.css" type="text/css"></style>-->
-    <!--  new changes 
-
-    <!-- If you'd like to support IE8 -->
+     <!-- If you'd like to support IE8 -->
         <script src="http://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
 </head>
 <body layout="column" ng-cloak>
@@ -138,34 +114,23 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.js"></script>
 <script type="text/javascript" src="bower_components/jqueryui-touch-punch/jquery.ui.touch-punch.min.js"></script>
-<script type="text/javascript" src="scripts/controllers/jspdf.min.js"></script>
 <script type="text/javascript" src="http://html2canvas.hertzen.com/build/html2canvas.js"></script>
 <script type="text/javascript" src="scripts/vendor/jquery.jsPlumb-1.4.1-all-min.js"></script>
 <script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 <script type="text/javascript" src="bower_components/bootstrap-colorpicker/js/bootstrap-colorpicker.js"></script>
 <script type="text/javascript" src="scripts/vendor/twitteroauth.js"></script>
-<!-- <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-animate.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.3.15/angular-aria.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angular_material/0.9.4/angular-material.min.js"></script> -->
 <script src="bower_components/angular-ui/angular-ui-select2/select2.min.js"></script>
-
 <script type="text/javascript" src="bower_components/angular/angular.min.js"></script>
 <script type="text/javascript" src="bower_components/angular-animate/angular-animate.js"></script>
 <script type="text/javascript" src="bower_components/angular-aria/angular-aria.js"></script>
 <script type="text/javascript" src="bower_components/angular-material/angular-material.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/angular-material-icons/0.6.0/angular-material-icons.min.js"></script>
-
-<script src="http://code.highcharts.com/highcharts.js"></script>
- 
+<script src="http://code.highcharts.com/highcharts.js"></script> 
 <script type="text/javascript" src="scripts/vendor/highcharts-ng.js"></script>
 <script src="http://code.highcharts.com/modules/exporting.js"></script>
 <script type="text/javascript" src="http://code.highcharts.com/modules/drilldown.js"></script>
 <script type="text/javascript" src="scripts/vendor/google-picker.min.js"></script>
-<script type="text/javascript" src="http://code.highcharts.com/highcharts-3d.js"></script>
-<!-- <script type="text/javascript" src="scripts/vendor/Googleinitial.js"></script>  -->
 <script type="text/javascript" src="http://d3js.org/d3.v3.min.js"></script>
-<script type="text/javascript" src="https://rawgit.com/emeeks/3361332/raw/61cf57523fe8cf314333e5f60cc266351fec2017/d3.layout.cloud.js"></script>
 <script src="http://dimplejs.org/dist/dimple.v2.1.6.min.js"></script>
 <script type="text/javascript" src="bower_components/underscore/underscore.js"></script>
 <script type="text/javascript" src="scripts/config.js"></script>
@@ -178,12 +143,9 @@
 <script type="text/javascript" src="bower_components/notification/ng-Toast/ngToast.min.js"></script>
 <script type="text/javascript" src="bower_components/v-accordion/v-accordion.min.js"></script>
 <script type="text/javascript" src="bower_components/ngSlimscroll/src/js/ngSlimscroll.js"></script>
-
 <!-- image crop -->
 <script type="text/javascript" src="bower_components/imgcrop/ng-img-crop.js"></script>
-
 <script src="bower_components/tag-input/ng-tags-input.min.js"></script>
-
 <!-- codemirror -->
 <script type="text/javascript" src="bower_components/codemirror/lib/codemirror.js"></script>
 <script type="text/javascript" src="bower_components/codemirror/addon/display/placeholder.js"></script>
@@ -201,21 +163,14 @@
 <script type="text/javascript" src="scripts/directives/calendar.js"></script>
 <script type="text/javascript" src="scripts/vendor/fullcalendar.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.9.0/gcal.js"></script>
-
 <!-- filesaver -->
-<script type="text/javascript"
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/98887/angular-fullscreen.js"></script>
 <script type="text/javascript"
         src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-router/0.2.13/angular-ui-router.min.js"></script>
 <script type="text/javascript" src="bower_components/ngstorage/ngStorage.js"></script>
-<script type="text/javascript" src="scripts/vendor/pivot.js"></script>
-<script type="text/javascript" src="bower_components/d3-plugins/sankey/sankey.js"></script>
-<script type="text/javascript" src="bower_components/d3-plugins/hexbin/hexbin.js"></script>
 <!-- PouchDB -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/pouchdb/5.4.5/pouchdb.js"></script>
 <script src="https://cdn.jsdelivr.net/angular.pouchdb/4.3.0/angular-pouchdb.min.js"></script>
 <!-- html2canvas -->
-
 <script type="text/javascript" src="scripts/custom/utility.js"></script>
 <script type="text/javascript" src="scripts/app.js"></script>
 <script type="text/javascript" src="scripts/controllers/welcomePageCtrl.js"></script>
@@ -223,7 +178,6 @@
 <script type="text/javascript" src="scripts/controllers/sourceAlgorithm/sourceAlgorithmCtrl.js"></script>
 <script type="text/javascript" src="scripts/controllers/help/videoCtrl.js"></script>
 <script type="text/javascript" src="scripts/controllers/data-source/dataSourceCtrl.js"></script>
-
 <!--download pdf-->
 <script src="https://rawgit.com/exupero/saveSvgAsPng/gh-pages/saveSvgAsPng.js"></script>
 <script src="https://rawgit.com/MrRio/jsPDF/master/dist/jspdf.debug.js"></script>
@@ -303,7 +257,6 @@
 <!--  controllers end   -->
 <!-- zeroclipboard -->
 <script type="text/javascript" src="bower_components/zeroclipboard/ZeroClipboard.min.js"></script>
-<script type="text/javascript" src="scripts/factories/d3Data.js"></script>
 <script type="text/javascript" src="scripts/factories/Dashboard.js"></script>
 <script type="text/javascript" src="scripts/factories/getTableDataFactories.js"></script>
 <script type="text/javascript" src="scripts/services/RealTimeService.js"></script>
@@ -316,7 +269,7 @@
 <script type="text/javascript" src="scripts/custom/diginservicehandler.js"></script>
 <script type="text/javascript" src="scripts/factories/aggregator.js"></script>
 <script type="text/javascript" src="scripts/controllers/widgetCtrl.js"></script>
-<script type="application/javascript" src="scripts/controllers/CommonDataSrcCtrl.js"></script>
+<script type="text/javascript" src="scripts/controllers/CommonDataSrcCtrl.js"></script>
 <script type="text/javascript" src="scripts/controllers/ExcelCtrl.js"></script>
 <script type="text/javascript" src="scripts/controllers/sharedashboardgroupsCtrl.js"></script>
 
@@ -364,29 +317,8 @@
 
 <script src="views/share-dataset/showFileShareDetailsCtrl.js"></script>
 <script src="views/share-dataset/shareDataSetCtrl.js"></script>
-
-<script src="scripts/charts/d3Force.js"></script>
-
-<script src="scripts/charts/streamgraph.js"></script>
-<script src="scripts/charts/scatterPlot.js"></script>
-<script src="scripts/charts/packing.js"></script>
-<script src="scripts/charts/clusterDendrogram.js"></script>
-<script src="scripts/charts/voronoi.js"></script>
-<script src="scripts/charts/delaunay.js"></script>
-<script src="scripts/charts/alluvial.js"></script>
-<script src="scripts/charts/clusterForce.js"></script>
-<script src="scripts/charts/convexHull.js"></script>
-<script src="scripts/charts/hexagonalBinning.js"></script>
-<script src="scripts/charts/reingoldTilford.js"></script>
-<script src="scripts/charts/parallelCoordinates.js"></script>
-<script src="scripts/charts/circularDendrogram.js"></script>
-<script src="scripts/charts/smallMultiplesArea.js"></script>
-<script src="scripts/charts/bumpChart.js"></script>
-<script src="scripts/charts/sunBurst.js"></script>
-<script src="scripts/services/services.js"></script>
-<script type="text/javascript" src="scripts/controllers/controllers.js"></script>
-<script type="text/javascript" src="scripts/filters/filters.js"></script>
-<script type="text/javascript" src="scripts/directives/directives.js"></script>
+<script src="scripts/charts/d3Force.js"></script>  
+<script src="scripts/charts/sunBurst.js"></script> 
 <script type="text/javascript" src="scripts/directives/queryBuilderDirective.js"></script>
 <!--hight charts update damith -->
 <!-- // <script type="text/javascript" src="bower_components/highcharts/highcharts.js"></script> -->
@@ -411,8 +343,7 @@
 <script type="text/javascript" src="scripts/md-steppers.js"></script>
 
 <!-- zeroclipboard -->
-<script type="text/javascript"
-        src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/98887/angular-fullscreen.js"></script>
+
 <script type="text/javascript" src="bower_components/angular-strap/dist/angular-strap.min.js"></script>
 <script type="text/javascript" src="bower_components/angular-ui/build/angular-ui.min.js"></script>
 <script type="text/javascript"
@@ -429,23 +360,7 @@
  <script src="http://code.highcharts.com/mapdata/custom/world.js"></script>
  <script type="text/javascript" src="scripts/vendor/lk-all.js"></script>
 <script  type="text/javascript" src="http://code.highcharts.com/mapdata/custom/asia.js"></script>
-<script  type="text/javascript" src="http://code.highcharts.com/mapdata/countries/us/us-all.js"></script>
-<script  type="text/javascript" src="http://code.highcharts.com/mapdata/custom/oceania.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/custom/europe.js"></script>
-<script  type="text/javascript" src="http://code.highcharts.com/mapdata/custom/north-america.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/custom/south-america.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/id/id-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/in/in-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/al/al-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/dz/dz-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/as/as-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/ad/ad-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/ao/ao-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/ag/ag-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/am/am-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/au/au-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/at/at-all.js"></script>
-<script type="text/javascript"  src="http://code.highcharts.com/mapdata/countries/az/az-all.js"></script>
+<script  type="text/javascript" src="http://code.highcharts.com/mapdata/countries/us/us-all.js"></script> 
 <script type="text/javascript" src="bower_components/google-map/angular-simple-logger.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/angular-google-maps/2.3.3/angular-google-maps.min.js"></script>
 <!-- marker clusterer -->
@@ -468,7 +383,7 @@
 <!-- angular datepicker -->
 <script src="scripts/controllers/angular-datepicker.js"></script>
 <!-- linq for quering in elastic widget -->
-<script type="text/javascript" src="scripts/vendor/linq/linq.js"></script>
+ 
 <script type="text/javascript" src="bower_components/angular-utils-pagination/dirPagination.js"></script>
 
 <script>

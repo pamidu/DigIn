3fed69995511a0ef3259bc8df9967076e6d5412c
Chamila <chamila@duosoftware.com>
2017-01-30 12:24:50 +0530

    Romove blocking validation for loggin

--- shell/scripts/config.js
+++ shell/scripts/config.js
@@ -10,8 +10,7 @@ angular.module('configuration', [])
 .constant('Digin_ObjStore', 'http://dev.auth.digin.io:3000') //#http://dev.auth.digin.io:3000
 //#for loggin process
 .constant('IsLocal', true) //# true for local developmet only 
-.constant('Local_Shell_Path', 'http://localhost:8081/DigIn/shell') //#This is the shell path only for local usage
-.constant('report_Widget_Iframe', 'http://dev.digin.io/Reports/HourlyCallSummery_HBL/HourlyCallSummery_HBL.pdf')
+.constant('Local_Shell_Path', 'http://localhost:8080/DigIn/shell') //#This is the shell path only for local usage
 //#apis folder path inside the html folder ** NOTE : if apis folder in -->html/apis/ then apis_path--> '/apis/'
 .constant('apis_Path', '/apis/')
 .constant('auth_Path', '/auth/')

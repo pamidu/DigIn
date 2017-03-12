65b3634b13724511fbdb2c83c1bc6f87813375f0
gevindu1990 <gevindu@duosoftware.com>
2017-03-12 18:40:50 +0530

    increment version

--- shell/scripts/config.js
+++ shell/scripts/config.js
@@ -19,7 +19,7 @@ angular.module('configuration', [])
 //#for onsite implementation (Note : for on prem  version onsite=true, for cloud version onsite=false)
 .constant('onsite', true)   
 .constant('tenantId','test')
-.constant('version','V3.2.1.5')
+.constant('version','V3.2.1.6')
 //#Database type --> mssql, postgresql,bigquery,mysql #Database type for csv/excel upload
 .constant('dbType', 'memsql')
 //# url for facebook app

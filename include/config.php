6e2af3da6b0b8063e8e9b3add4a5c8b529f8b15c
dillyboy <nelly.dil@hotmail.com>
2017-03-23 10:43:00 +0530

    config change

--- include/config.php
+++ include/config.php
@@ -1,5 +1,5 @@
 <?php
-	/*
+	
 	$mainDomain="dev.digin.io";
 	$authURI="http://dev.auth.digin.io:3048/"; //#Note : http://dev.auth.digin.io:3048/
 	$objURI="http://dev.auth.digin.io:3000/"; //#Note : http://dev.auth.digin.io:3000/
@@ -15,8 +15,8 @@
 	define("Digin_Engine_API", "http://dev.digin.io/DigInEngine/"); //Note : http://dev.digin.io/DigInEngine/
 	define("PAYMENT_GATWAY", "stripe"); 
 	define("onsite", false);
-	*/
 
+	/*
 	$mainDomain="ddigin.io";
 	$authURI="http://prod.proxy.auth.digin.io/"; //#Note : http://dev.auth.digin.io:3048/
 	$objURI="http://prod.proxy.auth.digin.io:3000/"; //#Note : http://dev.auth.digin.io:3000/
@@ -32,5 +32,5 @@
 	define("Digin_Engine_API", "http://prod.proxy.diginengine.digin.io/"); //Note : http://dev.digin.io/DigInEngine/
 	define("PAYMENT_GATWAY", "stripe"); 
 	define("onsite", false); 
-
+	*/
 ?>
--- shell/scripts/config.js
+++ shell/scripts/config.js
@@ -1,35 +1,35 @@
-// angular.module('configuration', [])
-// .constant('Digin_Engine_API', 'http://dev.digin.io/DigInEngine/') //#Note : http://dev.digin.io/DigInEngine/
-// .constant('Digin_Engine_API_Namespace', 'Demo')
-// .constant('Digin_Tomcat_Base', 'http://dev.digin.io:9897/') //#Note : http://dev.digin.io:9897/
-// .constant('Digin_Domain', 'dev.digin.io') //# Note : dev.digin.io
-// .constant('DevStudio', false)
-// .constant('ReportDevelopment','http://prod.report.digin.io:5488/') //#Note : http://prod.report.digin.io:5488/
-// .constant('Digin_Auth', 'http://dev.digin.io/') //# Note : http://dev.digin.io/
-// .constant('Digin_Tenant', 'http://dev.auth.digin.io:3048') //#Note : http://dev.auth.digin.io:3048
-// .constant('Digin_ObjStore', 'http://dev.auth.digin.io:3000') //#http://dev.auth.digin.io:3000
-// //#for loggin process
-// .constant('IsLocal', true) //# true for local developmet only
-// .constant('DeveloperMode', true) //# true for local developmet only 
-// .constant('Local_Shell_Path', 'http://localhost:8081/DigIn/cleanshell') //#This is the shell path only for local usage
-// //#apis folder path inside the html folder ** NOTE : if apis folder in -->html/apis/ then apis_path--> '/apis/'
-// .constant('apis_Path', '/apis/')
-// .constant('auth_Path', '/auth/')
-// .constant('include_Path', '/include/')
-// //#for onsite implementation (Note : for on prem  version onsite=true, for cloud version onsite=false)
-// .constant('onsite', true)   
-// .constant('tenantId','test')
-// .constant('version','V3.2.1.6')
-// //#Database type --> mssql, postgresql,bigquery,mysql #Database type for csv/excel upload
-// .constant('dbType', 'memsql')
-// //# url for facebook app
-// .constant('fbUrl', 'http://localhost:8081/DigIn-FB/index.html')
+angular.module('configuration', [])
+.constant('Digin_Engine_API', 'http://dev.digin.io/DigInEngine/') //#Note : http://dev.digin.io/DigInEngine/
+.constant('Digin_Engine_API_Namespace', 'Demo')
+.constant('Digin_Tomcat_Base', 'http://dev.digin.io:9897/') //#Note : http://dev.digin.io:9897/
+.constant('Digin_Domain', 'dev.digin.io') //# Note : dev.digin.io
+.constant('DevStudio', false)
+.constant('ReportDevelopment','http://prod.report.digin.io:5488/') //#Note : http://prod.report.digin.io:5488/
+.constant('Digin_Auth', 'http://dev.digin.io/') //# Note : http://dev.digin.io/
+.constant('Digin_Tenant', 'http://dev.auth.digin.io:3048') //#Note : http://dev.auth.digin.io:3048
+.constant('Digin_ObjStore', 'http://dev.auth.digin.io:3000') //#http://dev.auth.digin.io:3000
+//#for loggin process
+.constant('IsLocal', true) //# true for local developmet only
+.constant('DeveloperMode', true) //# true for local developmet only 
+.constant('Local_Shell_Path', 'http://localhost:81/DigIn/cleanshell') //#This is the shell path only for local usage
+//#apis folder path inside the html folder ** NOTE : if apis folder in -->html/apis/ then apis_path--> '/apis/'
+.constant('apis_Path', '/apis/')
+.constant('auth_Path', '/auth/')
+.constant('include_Path', '/include/')
+//#for onsite implementation (Note : for on prem  version onsite=true, for cloud version onsite=false)
+.constant('onsite', true)   
+.constant('tenantId','test')
+.constant('version','V3.2.1.6')
+//#Database type --> mssql, postgresql,bigquery,mysql #Database type for csv/excel upload
+.constant('dbType', 'memsql')
+//# url for facebook app
+.constant('fbUrl', 'http://localhost:8081/DigIn-FB/index.html')
 
-// //configure datasource connections in DigIn/shell/jsons/dbConfig.json
-// // #set the 'display' parameter in each object to configure visibility
-// // # "display": "true" - Datasource will be visible
-// // # "display": "false" - Datasource will be hidden
-
+//configure datasource connections in DigIn/shell/jsons/dbConfig.json
+// #set the 'display' parameter in each object to configure visibility
+// # "display": "true" - Datasource will be visible
+// # "display": "false" - Datasource will be hidden
+/*
 angular.module('configuration', [])
 .constant('Digin_Engine_API', 'http://prod.proxy.diginengine.digin.io/') //#Note : http://dev.digin.io/DigInEngine/
 .constant('Digin_Engine_API_Namespace', 'Demo')
@@ -61,3 +61,4 @@ angular.module('configuration', [])
 // #set the 'display' parameter in each object to configure visibility
 // # "display": "true" - Datasource will be visible
 // # "display": "false" - Datasource will be hidden
+*/

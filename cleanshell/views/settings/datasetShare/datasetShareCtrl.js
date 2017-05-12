0e4a2d1732f90407748d1f13fd45c7be5ac6fa30
gevindu1990 <gevindu@duosoftware.com>
2017-05-12 10:50:50 +0530

    dataset share and delete

--- cleanshell/views/settings/datasetShare/datasetShareCtrl.js
+++ cleanshell/views/settings/datasetShare/datasetShareCtrl.js
@@ -1,11 +1,12 @@
-DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','notifications','$http','Digin_Engine_API','Digin_Domain','$state','ProfileService','userAdminFactory','apis_Path','dbType',function ($scope,$rootScope,$mdDialog,notifications,$http,Digin_Engine_API,Digin_Domain,$state,ProfileService,userAdminFactory,apis_Path,dbType){
+DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','notifications','$http','Digin_Engine_API','Digin_Domain','$state','apis_Path','dbType',function ($scope,$rootScope,$mdDialog,notifications,$http,Digin_Engine_API,Digin_Domain,$state,apis_Path,dbType){
 
   $scope.$parent.currentView = "Share Dataset";
 	$scope.step1 = {};
+  $scope.selected = 0;
     $scope.goToNextStep = function()
     {
         $scope.selected = 1;
-		$scope.step1.completed = true;
+		    $scope.step1.completed = true;
     }
  
     $scope.files = [];
@@ -29,7 +30,7 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
       $scope.selectedFiles = [];
       $scope.selectedFolders = [];
 
-       $http.get(Digin_Engine_API+'GetTables?db='+dbType+'&SecurityToken='+userInfo.SecurityToken+'')
+       $http.get(Digin_Engine_API+'GetTables?db='+dbType+'&SecurityToken='+userInfo.SecurityToken)
            .then(function(result) {
                 for(var i = 0; i < result.data.Result.length; i++){
                     if(result.data.Result[i].upload_type == "csv-singlefile"){
@@ -37,6 +38,8 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
                     }else{
                       $scope.folders.push(result.data.Result[i]);
                     }
+
+                    console.log($scope.files);
 
                 }
             },function errorCallback(response) {
@@ -137,7 +140,7 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
     //------------------------ Users and groups goes here -----------------------------------------------------
 
 
-    $scope.users = [];
+    $scope.users = [{"Id":"aaaa@gmail.com","Name":"s s","UserID":"748dcb8020d7551cbed959cdc3c6062c","Type":"User","$$hashKey":"object:1114"},{"Id":"binara@duosoftware.com","Name":"Binara Goonawardana","UserID":"7c2276119d72a04e0faf3866ef745a1a","Type":"User","$$hashKey":"object:1115"},{"Id":"chamila","Name":"Chamia Dilhani","UserID":"07fe018feb51516eb6a1e76c9c41bc11","Type":"User","$$hashKey":"object:1116"},{"Id":"chamila1","Name":"Chamila Dilhani","UserID":"3ba3818a9f0475af29f2004340e41c5c","Type":"User","$$hashKey":"object:1117"},{"Id":"chamila2","Name":"Chamila Dilhani","UserID":"be21c1beb48dc2b42218abf23003afc8","Type":"User","$$hashKey":"object:1118"},{"Id":"chamila890","Name":"Chamila Abeykoon","UserID":"8e2261a81fe1f58e46eff54f980f134f","Type":"User","$$hashKey":"object:1119"},{"Id":"dilhani","Name":"Chamila Dilhani","UserID":"3e48b235f1ccfde15473ef21ce0dee04","Type":"User","$$hashKey":"object:1120"},{"Id":"dilshan@duosoftware.com","Name":"Dilshan Liyanage","UserID":"b90cdf017ba935f6696842fe846e36fa","Type":"User","$$hashKey":"object:1121"},{"Id":"dinesh@duosoftware.com","Name":"Dinesh Priyankara","UserID":"342fd2a81902b425eb59f5594e495ef8","Type":"User","$$hashKey":"object:1122"},{"Id":"gevindu","Name":"Gevindu Mallikarachchi","UserID":"260ee05289098e09b65a111ef5515977","Type":"User","$$hashKey":"object:1123"},{"Id":"gevindu1","Name":"Givindu Mallikarachchi","UserID":"ca7a5648f318f37b7f0c6d9fb7176cba","Type":"User","$$hashKey":"object:1124"},{"Id":"ian","Name":"Ian T","UserID":"a444a8099ac14c854a4be694219bbef6","Type":"User","$$hashKey":"object:1125"},{"Id":"marlon","Name":"Chamila Dilhani","UserID":"0bbbab669df59fcc18dc83a1e8e9e203","Type":"User","$$hashKey":"object:1126"},{"Id":"marlon123","Name":"gevindu 123","UserID":"86cbde1211ceb9eaae0f03689247ea1d","Type":"User","$$hashKey":"object:1127"},{"Id":"michel","Name":"Michel Phelps","UserID":"220595d3a2e42514e950bacdbc0a7155","Type":"User","$$hashKey":"object:1128"},{"Id":"moyyugthvzjt@dropmail.me","Name":"Chamila 1","UserID":"691a0c7965acdb77ce043bfd7fd73a71","Type":"User","$$hashKey":"object:1129"},{"Id":"nadun001","Name":"Nadun Dhanushka","UserID":"dbab007e136278e7af8b0068a7f255db","Type":"User","$$hashKey":"object:1130"},{"Id":"nadun01","Name":"Nadun Dhanushka","UserID":"69eba33db25aa9027c365020cbef25ca","Type":"User","$$hashKey":"object:1131"},{"Id":"nadun1","Name":"Nadun Dhanushka","UserID":"a2809bb80bcecc2989ee4834045e9258","Type":"User","$$hashKey":"object:1132"},{"Id":"ndk@10host.top","Name":"Nadun Dhanushka","UserID":"e90cc4cfb3f7178e9d1d3624d92f5ffe","Type":"User","$$hashKey":"object:1133"},{"Id":"nwjbqyea@10mail.org","Name":"chamila dilhani","UserID":"160f3a751aafcfdcad11db5343fe3ba9","Type":"User","$$hashKey":"object:1134"},{"Id":"rangika","Name":"Rangika Perera","UserID":"26e221fbd9e2a184932fdddaefc60ebf","Type":"User","$$hashKey":"object:1135"},{"Id":"rangika.net@gmail.com","Name":"Rangika Perera","UserID":"d848378c621c644c36299727c9e4a961","Type":"User","$$hashKey":"object:1136"},{"Id":"sd@g.pv","Name":"sdf dfdsf","UserID":"ea09f89c2c6df79c25881aaec5ac174d","Type":"User","$$hashKey":"object:1137"},{"Id":"shane","Name":"Shane Watson","UserID":"4816752c34f75450bdf847c040027c9c","Type":"User","$$hashKey":"object:1138"},{"Id":"steve","Name":"Steve J","UserID":"a754693ea054eaeea6a4252331e89e2e","Type":"User","$$hashKey":"object:1139"},{"Id":"steveroger@gmail.com","Name":"Steve Roger","UserID":"9c45a76f06923a93738d1eeb01e67cf0","Type":"User","$$hashKey":"object:1140"}];
     $scope.Allusers = [];
     $scope.groups = [];
 
@@ -243,7 +246,7 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
           if(file.shared_by == null || file.shared_by ==""){
              $mdDialog.show({
                   controller: 'showFileShareDetailsCtrl',
-                  templateUrl: 'views/share-dataset/showFileShareDetails.html',
+                  templateUrl: 'views/settings/datasetShare/showFileShareDetails.html',
                   resolve: {},
                   locals: {
                       users: $scope.Allusers,
@@ -294,7 +297,7 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
                                   'SecurityToken': userInfo.SecurityToken
                       }
                   })
-                  .success(function(response){
+                  .then(function(response){
                      
                       if(response.Is_Success == false){
                          notifications.toast(0, response.Custom_Message);
@@ -315,11 +318,9 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
 
                     }
 
-                  })
-                  .error(function(error){  
-                    notifications.toast(0, "Error occurred during share, please try again");
-                   
-                  });
+                  },function errorCallback(response) {
+                          notifications.toast(0, "Error occurred during share, please try again");
+                   }); 
 
             }else{
               notifications.toast(0, "Your trying to share with already shared users or you dont have permition to share");
@@ -432,6 +433,83 @@ DiginApp.controller('datasetShareCtrl', ['$scope','$rootScope','$mdDialog','noti
             }
         return name;
     }
+
+            $scope.DeleteFilesFolders = function(){
+
+         if($scope.selectedFiles.length > 0 || $scope.selectedFolders.length > 0 ){
+                      var confirm = $mdDialog.confirm()
+                      .title('Delete Data Set')
+                      .textContent('Would you like to delete the data set?')
+                      .ariaLabel('')
+                      .targetEvent()
+                      .ok('Yes')
+                      .cancel('No');
+
+                        $mdDialog.show(confirm).then(function() {
+
+                                //get delete files
+                                    var deleteObj=[];
+                                    for(var i = 0; i< $scope.selectedFiles.length; i++){
+                                        
+                                        var obj = {
+                                            "file_type":"single",
+                                            "datasource_id":$scope.selectedFiles[i].datasource_id,
+                                            "shared_user_Id":$scope.selectedFiles[i].shared_by,
+                                            "deletion_type":"permanent"
+                                        };
+                                        deleteObj.push(obj);
+                                    }
+
+
+                                    //get delete folders
+                                    for(var i = 0; i< $scope.selectedFolders.length; i++){
+                                        
+                                        var obj = {
+                                            "file_type":"directory",
+                                            "datasource_id":$scope.selectedFolders[i].datasource_id,
+                                            "shared_user_Id":$scope.selectedFolders[i].shared_by,
+                                            "deletion_type":"permanent"
+                                        };
+                                        deleteObj.push(obj);
+                                    }
+
+                                  var userInfo= JSON.parse(decodeURIComponent(getCookie('authData')));
+                                  $http({
+                                      method: 'POST',
+                                      
+                                      url: Digin_Engine_API +'datasource_delete',
+                                      data: angular.fromJson(JSON.stringify(deleteObj)),
+                                      headers: {  
+                                                  'Content-Type': 'application/json',
+                                                  'SecurityToken': userInfo.SecurityToken
+                                      }
+                                  }),then(function(response){
+                                     
+                                      if(response.Is_Success == false){
+                                         notifications.toast(0, response.Custom_Message);
+                                    }
+                                    else{
+                                        notifications.toast(1, response.Custom_Message);
+                                        $scope.loadFilesFolder();
+                                        $scope.selectedFolders = [];
+                                        $scope.selectedFiles = [];
+                                        
+                                    }
+
+                                  },function errorCallback(response) {
+                                          notifications.toast(0, "Error occurred during deletion, please try again");
+                                  }); 
+                        }, function() {
+                   
+                             
+                        });  
+          }else{
+              notifications.toast(0, "Please select files or folders you wish to Delete");
+          }
+                
+
+        }
+
 
 
 }]);
